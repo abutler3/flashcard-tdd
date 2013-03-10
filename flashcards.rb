ddab83f5a259dc1ecb6eb028a30af489e293cb3b
Drew Butler <abutler3@gmail.com>
2013-03-10 11:17:31 -0500

    Change to symbols

--- flashcards.rb
+++ flashcards.rb
@@ -1,14 +1,14 @@
 
 card1 = {
-  "front" => "cat", "back" => "neko"
+  front: "cat", back: "neko"
   }
 card2 = {
-  "front" => "dog", "back" => "inu"
+  front: "dog", back: "inu"
   }
 deck = [card1, card2]
 deck.each do |card|
-  front = card["front"]
-  back = card["back"]
+  front = card[:front]
+  back = card[:back]
 
   print "#{front} > "
   guess = gets.chomp
