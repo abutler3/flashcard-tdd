12a7d79fa47b2056cb793023ccf70b79988544d7
Drew Butler <abutler3@gmail.com>
2013-03-10 12:02:08 -0500

    Add attr_accessible to Card class

--- flashcards.rb
+++ flashcards.rb
@@ -1,11 +1,31 @@
 class Card
+  attr_accessor :front, :back
+
   def initialize(front, back)
-    puts "#{front} => #{back}"
+    @front = front
+    @back = back
   end
+
+  # def front replaced by attr_reader
+  #   @front
+  # end
+
+  # def back replaced by attr_reader
+  #   @back
+  # end
+
+  # def front= front replaced by attr_writer
+  #   @front = front
+  # end
+  #
 end
 
 card1 = Card.new("cat", "neko")
 card2 = Card.new("dog", "inu")
+# card1.front = "Cat"
+# puts card1.front
+
+
 
 # card1 = {
 #   front: "cat", back: "neko"
@@ -13,18 +33,18 @@ card2 = Card.new("dog", "inu")
 # card2 = {
 #   front: "dog", back: "inu"
 #   }
-# deck = [card1, card2]
-# deck.each do |card|
-#   front = card[:front]
-#   back = card[:back]
+deck = [card1, card2]
+deck.each do |card|
+  front = card.front
+  back = card.back
 
-#   print "#{front} > "
-#   guess = gets.chomp
+  print "#{front} > "
+  guess = gets.chomp
 
 
-# if guess == back
-#   puts "Correct"
-# else
-#   puts "Incorrect. The answer was #{back}."
-# end
-# end
+if guess == back
+  puts "Correct"
+else
+  puts "Incorrect. The answer was #{back}."
+end
+end
