fe0275404cc9bcccaa1b752a9929855ec0bf34ad
Drew Butler <abutler3@gmail.com>
2013-03-10 11:34:41 -0500

    Add Card class

--- flashcards.rb
+++ flashcards.rb
@@ -1,22 +1,30 @@
-
-card1 = {
-  front: "cat", back: "neko"
-  }
-card2 = {
-  front: "dog", back: "inu"
-  }
-deck = [card1, card2]
-deck.each do |card|
-  front = card[:front]
-  back = card[:back]
-
-  print "#{front} > "
-  guess = gets.chomp
-
-
-if guess == back
-  puts "Correct"
-else
-  puts "Incorrect. The answer was #{back}."
+class Card
+  def initialize(front, back)
+    puts "#{front} => #{back}"
+  end
 end
-end
+
+card1 = Card.new("cat", "neko")
+card2 = Card.new("dog", "inu")
+
+# card1 = {
+#   front: "cat", back: "neko"
+#   }
+# card2 = {
+#   front: "dog", back: "inu"
+#   }
+# deck = [card1, card2]
+# deck.each do |card|
+#   front = card[:front]
+#   back = card[:back]
+
+#   print "#{front} > "
+#   guess = gets.chomp
+
+
+# if guess == back
+#   puts "Correct"
+# else
+#   puts "Incorrect. The answer was #{back}."
+# end
+# end
